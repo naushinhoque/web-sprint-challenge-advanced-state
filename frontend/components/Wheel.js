e7413e51c9071530b0da8d5cbec14b8bc356cb0b
naushinhoque <fnmh@yahoo.com>
2023-09-09 16:39:13 -0500

    wheel

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { React, useState} from 'react'
 
 export default function Wheel(props) {
+
+  const [rotationAngle, setRotationAngle] = useState(0);
+
+  const rotateClockwise = () => {
+    setRotationAngle( rotationAngle + 60 );
+  };
+
+  const rotateCounterClockwise = () => {
+    setRotationAngle( rotationAngle - 60 );
+  };
+
   return (
     <div id="wrapper">
-      <div id="wheel">
+      <div id="wheel" style={{ transform: `rotate(${rotationAngle}deg)`}}>
         <div className="cog active" style={{ "--i": 0 }}>B</div>
         <div className="cog" style={{ "--i": 1 }}></div>
         <div className="cog" style={{ "--i": 2 }}></div>
@@ -12,8 +23,8 @@ export default function Wheel(props) {
         <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
-        <button id="counterClockwiseBtn" >Counter clockwise</button>
-        <button id="clockwiseBtn">Clockwise</button>
+        <button id="counterClockwiseBtn" onClick={rotateClockwise}>Counter clockwise</button>
+        <button id="clockwiseBtn" onClick={rotateCounterClockwise}>Clockwise</button>
       </div>
     </div>
   )
