5bb4ea9b5c9260a2f45d027e77338ad9a85799f4
naushinhoque <fnmh@yahoo.com>
2023-09-15 13:38:19 -0500

    done

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,51 +1,62 @@
-import React, { useState } from 'react'
-import { connect } from 'react-redux'
-import * as actionCreators from '../state/action-creators'
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actionCreators from '../state/action-creators';
 
 export function Form(props) {
-  const [formData, setFormData] = useState({
-    newQuestion: '',
-    newTrueAnswer: '',
-    newFalseAnswer: ''
-  });
-  //const { newQuestion, newTrueAnswer, newFalseAnswer } = formData;
+  const { formData, inputChange, postQuiz } = props;
 
   const onChange = evt => {
     const { id, value } = evt.target;
-    console.log(id, value);
-    setFormData({
-      ...formData,
-      [id]: value
-    });
+    inputChange(id, value);
   };
 
   const onSubmit = evt => {
     evt.preventDefault();
-    console.log(formData);
-    props.postQuiz(formData);
+    postQuiz(formData);
 
-    setFormData({
-      newQuestion: '',
-      newTrueAnswer: '',
-      newFalseAnswer: ''
-    });
+    // No need to reset the form data here; Redux will manage it.
   };
 
   const isDisabled = () => {
     const { newQuestion, newTrueAnswer, newFalseAnswer } = formData;
     return ![newQuestion, newTrueAnswer, newFalseAnswer].every(value => value.trim().length >= 2);
-    //return Object.values(formData).some(value => !value.trim().length)
-  }
+  };
 
   return (
     <form id="form" onSubmit={onSubmit}>
       <h2>Create New Quiz</h2>
-      <input value={formData.newQuestion} maxLength={50} onChange={onChange} id="newQuestion" placeholder="Enter question" />
-      <input value={formData.newTrueAnswer} maxLength={50} onChange={onChange} id="newTrueAnswer" placeholder="Enter true answer" />
-      <input value={formData.newFalseAnswer} maxLength={50} onChange={onChange} id="newFalseAnswer" placeholder="Enter false answer" />
-      <button id="submitNewQuizBtn" type="submit" disabled={isDisabled()}>Submit new quiz</button>
+      <input
+        value={formData.newQuestion}
+        maxLength={50}
+        onChange={onChange}
+        id="newQuestion"
+        placeholder="Enter question"
+      />
+      <input
+        value={formData.newTrueAnswer}
+        maxLength={50}
+        onChange={onChange}
+        id="newTrueAnswer"
+        placeholder="Enter true answer"
+      />
+      <input
+        value={formData.newFalseAnswer}
+        maxLength={50}
+        onChange={onChange}
+        id="newFalseAnswer"
+        placeholder="Enter false answer"
+      />
+      <button id="submitNewQuizBtn" type="submit" disabled={isDisabled()}>
+        Submit new quiz
+      </button>
     </form>
-  )
+  );
 }
 
-export default connect(st => st, actionCreators)(Form)
+const mapStateToProps = state => {
+  return {
+    formData: state.form,
+  };
+};
+
+export default connect(mapStateToProps, actionCreators)(Form);
